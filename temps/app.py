35468e11574938b5dd0663dd0a2bc36649061706
Sergey-Usachev <72167337+Sergey-Usachev@users.noreply.github.com>
2021-05-07 16:21:52 +0300

    Update app.py

--- temps/app.py
+++ temps/app.py
@@ -1,9 +1,9 @@
-from flask import flask
-application = flask(__name__)
+from flask import Flask
+application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World Usachev!"
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.run()

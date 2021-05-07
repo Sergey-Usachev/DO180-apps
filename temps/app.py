04255df3b5e234b23b91dbc11fb9d3305d960178
Sergey-Usachev <72167337+Sergey-Usachev@users.noreply.github.com>
2021-05-07 14:05:41 +0300

    Update app.py

--- temps/app.py
+++ temps/app.py
@@ -1,1 +1,9 @@
-print("Usachev text")
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return 'Hello World Усачев!'
+ 
+if __name__ == '__main__':
+    app.run()

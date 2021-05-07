9369d3abc545d4a0d4b86ba6e27b1bb4dc1c2765
Sergey-Usachev <72167337+Sergey-Usachev@users.noreply.github.com>
2021-05-07 12:36:36 +0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is Usachev Usachev Usachev console Пересборка Пересборка" . PHP_VERSION . "\n";
+print "Hello, World! php version is Usachev Usachev Usachev console Peresborka" . PHP_VERSION . "\n";
 ?>
